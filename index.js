dbf0db668e9f40e8ec8a95312c996f71b4926b0b
shawnbradyrobinson <shawnbradyrobinson1@gmail.com>
2023-05-12 20:43:16 -0500

    added some thoughts and ideas to different aspects of the starter code

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+/*
+    {
+        type: "input",
+        name: "project-name",
+        message: "what is your project's name?",
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "what is your name?",
+    },
+
+
+
+
+
+*/
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+/*
+const readme_File = data; 
+
+fs.writeFile(fileName, readme_File,(err) => err ? console.log(err) : console.log("Successfully created README.md!");
+
+*/
+
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+/**
+ * https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+ * 
+ * 
+ */
+
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
